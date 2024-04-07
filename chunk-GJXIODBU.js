f949f4706c7131abb2a58cf7b16c6f83ced74df2
Ftohtarek <ftohtarek48@gmail.com>
2024-04-07 12:47:52 +0000

    Deployed from sport-event-staging repository

---
+++ chunk-GJXIODBU.js
@@ -0,0 +1,1 @@
+import{b as o}from"./chunk-DMBEBI46.js";import{a as t}from"./chunk-6WC44PLZ.js";import{j as e}from"./chunk-R4HFWR2Y.js";var f=r=>{t.get(o).success("",r.message,{duration:5e3});let c=t.get(e);r.redirctTo&&c.navigate([r.redirctTo]),r.callback&&r.callback()};export{f as a};
