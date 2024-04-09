62918625e081f1641a52104ceedddb4e6992e906
Ftohtarek <ftohtarek48@gmail.com>
2024-04-09 03:26:18 +0000

    Deployed from sport-event-staging repository

---
+++ chunk-YKTD2NNP.js
@@ -0,0 +1,1 @@
+import{$ as o,Hb as s,kb as i,lb as a,yb as r}from"./chunk-TKMI5VCA.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-categories"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"categories works!"),a())}});let t=e;return t})();export{d as CategoriesComponent};
